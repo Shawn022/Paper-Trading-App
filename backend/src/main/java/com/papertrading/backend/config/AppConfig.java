4ad199d67ae10052b6556927a8fdb4af0d9a4eb0
Shawn022 <shawn58391@gmail.com>
2026-06-15 16:45:13 +0530

    Update AppConfig.java

--- backend/src/main/java/com/papertrading/backend/config/AppConfig.java
+++ backend/src/main/java/com/papertrading/backend/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.utils.Arrays;
+import java.util.Arrays;
 
 @Configuration
 public class AppConfig {
